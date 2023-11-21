b691c1f9142114ddaf628737076309ad5850cd90
RaphaLK <77198909+RaphaLK@users.noreply.github.com>
2023-11-20 20:11:16 -0800

    Created Resume Screen, improved about-me

--- src/pages/AboutMe.js
+++ src/pages/AboutMe.js
@@ -1,18 +1,47 @@
-import React from 'react';
-import '../styles/AboutMe.css';
+import React from "react";
+import "../styles/AboutMe.css";
+import teamPhoto from "../assets/IMG_6679.JPG";
 
 const AboutMe = () => {
   return (
-    <div className='InfoContainer'>
-      <h1 className='font-sans text-4xl font-bold mb-4 text-offWhite'>About Me</h1>
-      <p className='font-sans text-2xl text-offWhite'>My name is Raphael, I'm a Junior <span class='font-bold'>Computer Science and Engineering Major with a 
-        Minor in Mathematics</span> at <span class='font-bold'> Santa Clara University</span>. My current interests are in Front-End Development,
-        Mobile App Development. I've worked with Professors at Santa Clara University, and I currently work in SCU's very own Residence Life Department as an RA.
-        I'll expand more on my skills in the <span class='font-bold'>Resume</span> section. I'm currently looking for a <span class='font-bold'>Software Engineering Internship for Summer 2024</span>.
-        I love learning new things, I love <span class='font-bold font italic'><span class='font-serif'>Coffee, Boxing, Exercise, Photography, and Video Games</span></span>. 
-      </p>
+    <div>
+      <div className="InfoContainer">
+        <h1 className="font-sans text-4xl font-bold mb-4 text-offWhite">
+          About Me
+        </h1>
+        <p className="font-sans text-2xl text-offWhite">
+          My name is Raphael, I'm a Junior{" "}
+          <span class="font-bold">
+            Computer Science and Engineering Major with a Minor in Mathematics
+          </span>{" "}
+          at <span class="font-bold"> Santa Clara University</span>. My current
+          interests are in Full-Stack Development and Mobile App Development.
+          I've worked with Professors at Santa Clara University for projects,
+          and I currently work in SCU's very own Residence Life Department as an
+          RA. I'll expand more on my skills in the{" "}
+          <span class="font-bold">Resume</span> section. I'm currently looking
+          for a{" "}
+          <span class="font-bold">
+            Software Engineering Internship for Summer 2024
+          </span>
+          . I love learning new things, I love{" "}
+          <span class="font-bold font italic">
+            <span class="font-serif">
+              Coffee, Boxing, Exercise, Photography, and Video Games
+            </span>
+          </span>
+          .
+        </p>
+      </div>
+
+      <div className="imageContainer">
+        <img className="decorativePic1" src={teamPhoto} alt="" />
+        <div className="smallImageSnippet1">
+          <p className="text-offWhite bg-DarkishGreen p-1">me and my RA team :)</p>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default AboutMe;
--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -1,8 +1,40 @@
 import React from "react";
+import "../styles/Resume.css";
+import teamPhoto1 from "../assets/TeamPhoto.JPG";
 
 const Resume = () => {
   return (
-  <div>My Resume</div>
+    <div>
+      <div className="viewContainer">
+        <h1 className="font-sans text-4xl font-bold mb-4 text-offWhite">
+          My Skills
+        </h1>
+        <p className="font-sans text-2xl text-offWhite">
+          Here's what I can do :)
+        </p>
+        <div className="ExperienceItemContainer"></div>
+      </div>
+      <div className="viewContainer">
+        <h1 className="font-sans text-4xl font-bold mb-4 text-offWhite">
+          My Experience
+        </h1>
+        <p className="font-sans text-2xl text-offWhite">
+          Here's all my Jobs :)
+        </p>
+        <div className="ExperienceItemContainer">
+          <p>Community Facilitator/Neighborhood Representative/RA</p>
+          <img src={teamPhoto1} alt=''/>
+        </div>
+        <div className="ExperienceItemContainer">
+          <p>Mobile App Developer (Research for Dr.Navid Shaghaghi)</p>
+          <p></p>
+        </div>
+        <div className="ExperienceItemContainer">
+          <p>Library Student Assistant</p>
+          <p></p>
+        </div>
+      </div>
+    </div>
   );
 };
 
