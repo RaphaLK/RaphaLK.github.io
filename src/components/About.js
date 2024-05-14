5081712d74b8a3f63e626061eb6be69042cd5027
RaphaLK <77198909+RaphaLK@users.noreply.github.com>
2024-05-14 13:45:28 -0700

    styled about.js

--- src/components/About.js
+++ src/components/About.js
@@ -1,6 +1,7 @@
 import React from "react";
 import BackButton from "./ReturnToHome";
 import "../styles/AboutMe.css";
+import MyPhoto from "../assets/MyPhoto.jpg";
 
 const About = ({ backFunction }) => {
   return (
@@ -20,16 +21,15 @@ const About = ({ backFunction }) => {
         </div>
 
         <div className="grid grids-rows-4 grid-cols-2 mt-10">
-          <div className="align-middle">
+          <div className="Desc_Contain">
             <p className="myDescription">
-              Aspiring Software Engineer with experience in Full-Stack Web and Mobile App Development.
-              I'm currently learning about Edge AI/ML and Signal Processing for my Senior Design Project.
+              Aspiring Software Engineer with experience in <span className="font-bold italic">Full-Stack Web and Mobile App Development</span>.
+              I'm currently learning about <span className="font-bold italic"> Edge AI/ML Technologies and Signal Processing </span> for my Senior Design Project.
               We'll be using sEMG signals to provide accurate finger movements in VR/AR.
             </p>
-
           </div>
-          <div>
-            Photo Goes Here
+          <div className="ImageContainer">
+            <img src={MyPhoto} className="ImageStyling"/>
           </div>
 
         </div>
