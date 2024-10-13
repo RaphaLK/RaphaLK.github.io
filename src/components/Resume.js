851637d77aa32139c150057e5be74369fcd8412d
Raphael Kusuma <77198909+RaphaLK@users.noreply.github.com>
2024-10-12 20:46:05 -0700

    Added Descriptions

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -21,12 +21,13 @@ const Resume = ({ backFunction }) => {
           </p>
         </div>
         <Grid columns="3" gap="3" rows="repeat(2, 64px)" width="auto">
-          <ExperienceContainer companyName={"Anritsu"} roleName={"Software Engineering Intern"}/>
-          <ExperienceContainer companyName={"EPIC Lab SCU"} roleName={"Full Stack Mobile App Developer"}/>
-          <ExperienceContainer companyName={"Santa Clara University SWE++"} roleName={"Volunteer Tutor"}/>
-          <ExperienceContainer companyName={"Santa Clara University Residence Life"} roleName={"Resident Advisor"}/>
-          <ExperienceContainer companyName={"Springfield School Jakarta"} roleName={"Stage Manager/Stream Operator"}/>
-          <ExperienceContainer companyName={"Springfield School Jakarta"} roleName={"Head of Logistics"}/>
+          <ExperienceContainer companyName={"Anritsu"} roleName={"Software Engineering Intern"} description={"Worked on the Back-end of a Signal Generator and a prototype Vector Signal Generator."} date={"2024"}/>
+          <ExperienceContainer companyName={"EPIC Lab SCU"} roleName={"Full Stack Mobile App Developer"} description={"Developed key functionalities on a Mobile App focused on Public Transport use."} date={"2023 - Present"}/>
+          <ExperienceContainer companyName={"Santa Clara University SWE++"} roleName={"Volunteer Tutor"} description={"Taught Python students between the ages of middle school and highschool. Introduced them to projects and basic fundamentals of DSA."} date={"2024"}/>
+          <ExperienceContainer companyName={"Santa Clara University Residence Life"} roleName={"Resident Advisor"} description={"Organized events and handled logistics in the residence halls at SCU. Promoted community amongst the residents."} date={"2023-2024"}/>
+          <ExperienceContainer companyName={"Santa Clara University Library"} roleName={"Student Assistant"} description={"Provided customer service, organized Library layouts, handled Inter-Library Loan deliveries, and worked on material archival systems"} date={"2022-Present"}/>
+          <ExperienceContainer companyName={"Springfield School Jakarta"} roleName={"Stage Manager/Stream Operator"} description={"Handled the streaming and effect/prop timings of a Musical"} date={"2021"}/>
+          <ExperienceContainer companyName={"Springfield School Jakarta"} roleName={"Head of Logistics"} description={"Led a team to handle meals, competitions, equipment, and inter-team communication for a city-wide sports gathering"} date={"2020"}/>
         </Grid>
       </div>
     </div>
--- src/components/WorkCards/ExperienceContainer.js
+++ src/components/WorkCards/ExperienceContainer.js
@@ -7,32 +7,9 @@ const ExperienceContainer = ({companyName, roleName, date, description}) => {
     <Box>
       <Heading>{companyName}</Heading>
       <Text>{roleName}</Text>
+      <Text>{description}</Text>
+      <Text>{date}</Text>
     </Box>
-    /*
-    <Heading> Anritsu  </Heading>
-    <Text>Software Engineering Intern</Text>
-    <Heading> EPIC Lab -- Full Stack Mobile App Developer </Heading>
-    <Text>Full Stack Mobile App Developer</Text>
-    <Heading> Santa Clara University SWE++ </Heading>
-    <Text>Volunteer Tutor</Text>
-    <Heading>
-      {" "}
-      Santa Clara University Residence Life{" "}
-    </Heading>
-    <Text>Resident Advisor</Text>
-    <Heading>
-      {" "}
-      Santa Clara University Library -- Student Assistant{" "}
-    </Heading>
-    <Text></Text>
-    <Heading>
-      {" "}
-      Springfield School Jakarta -- Stage Manager/Stream Operator{" "}
-    </Heading>
-    <Text></Text>
-    <Heading> Springfield School Jakarta -- Head of Logistics </Heading>
-    <Text></Text>
-    */
   )
 }
 
